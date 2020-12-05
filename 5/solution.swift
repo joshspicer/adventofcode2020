36379dcc24f0fbc8bbd5c84c58b20245d3ea8031
joshspicer <hello@joshspicer.com>
2020-12-05 11:37:56 -0500

    day 5 part 1

---
+++ 5/solution.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+
+let data = try String(contentsOfFile: "./input", encoding: .utf8)
+let seats = data.components(separatedBy: .newlines)
+
+var highestSeat = 0
+for seat in seats {
+
+    let mid = seat.index(seat.startIndex, offsetBy: 7)
+
+    let first = String(seat[seat.startIndex..<mid])
+    // print(first)
+    let second = String(seat[mid..<seat.endIndex])
+    // print(second)
+
+    var fMask = Int(127)
+    var sMask = Int(7)
+
+    //FBFBBFF
+    //0101100
+    for (idx, char) in first.enumerated() {
+        if (char == "F") {
+            let exp = (2 << (6 - 1 - (idx)))
+            fMask = fMask & (127 - exp)
+        }
+    }
+
+    // RLR
+    // 101
+    for (idx, char) in second.enumerated() {
+        if (char == "L") {
+            let exp = (2 << (2 - 1 - (idx)))
+            sMask = sMask & (7 - exp)
+        }
+    }
+
+    let uniqueID = fMask * 8 + sMask
+    // print(uniqueID)
+    highestSeat = max(uniqueID, highestSeat)
+}
+
+print(highestSeat)
