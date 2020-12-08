f489fe06896f7c1acc5e3a27c5ac0da8d2f3c7d4
joshspicer <hello@joshspicer.com>
2020-12-08 09:57:55 -0500

    clean up

--- 8/solution.py
+++ 8/solution.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# mypy solution.py
-
 import re
 from typing import *
 import pprint
@@ -20,7 +18,6 @@ class Entry:
     def __str__(self) -> str:
         return f"[{self.idx}]{self.instruction} {self.value} {self.beenSeen}"
 
-
 parsedEntries: List[Entry] = []
 for idx, line in enumerate(file):
     split = line.strip().split(" ")
@@ -31,13 +28,13 @@ def execute(p2: bool, entries: List[Entry]):
     pc = 0
     acc = 0
     while True:
-        # print(f"pc={pc} acc={acc}")
         # Set current to program counter
         current = entries[pc]
 
         # Been seen
         if current.beenSeen:
-            # print(f"DONE: pc={pc} acc={acc}")
+            if not p2:
+                print(f"Loop Detected: pc={pc} acc={acc}")
             break
         else:
             current.beenSeen = True
@@ -45,7 +42,7 @@ def execute(p2: bool, entries: List[Entry]):
         # Execute instruction
         I = current.instruction
         V = current.value
-        # print(f"I={I} V={V}")
+
         if I == 'nop':
             pc += 1
         elif I == 'acc':
@@ -55,13 +52,15 @@ def execute(p2: bool, entries: List[Entry]):
             pc += V
     
         if p2 and pc == len(entries):
-            print(f"DONE {current.idx} {acc}")
+            print(f"Program Complete: idx={current.idx} acc={acc}")
             return
 
+# Part 1
+entries = copy.deepcopy(parsedEntries)
+execute(False, entries)
 
 # Part 2
 for i in range(0, len(parsedEntries)):
-
     entries = copy.deepcopy(parsedEntries)  
 
     if entries[i].instruction == "jmp":
