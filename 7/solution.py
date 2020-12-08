940b2df69966bd0c623f8d0b672cfd5fcacf9a16
joshspicer <hello@joshspicer.com>
2020-12-08 08:43:10 -0500

    day 7 part 2

--- 7/solution.py
+++ 7/solution.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+# mypy solution.py
+
 import re
-import typing
+from typing import *
 import pprint
 
 filename = "input"
@@ -10,19 +12,23 @@ file = open(filename, "r")
 re_1 = "^(.*) bags contain (.*).$"
 re_2 ="(.*\d) (.*) (bags?)"
 
-bags = {}
+bags : Dict[str, List[Tuple[int, str]]]= {}
+
 for line in file:
     v = re.match(re_1, line)
+    if not v:
+        print("No valid groups. Err.")
+        exit(1)
     outer = v.groups()[0].strip()
     inner = []
     for i in v.groups()[1].split(","):
         r = re.match(re_2, i)
         if not r:
             continue 
-        inner.append((r.groups()[0].strip(), r.groups()[1].strip()))
+        inner.append((int(r.groups()[0].strip()), r.groups()[1].strip()))
     bags[outer] = inner
 
-def count_can_contain_a_valid_bag(valid_bags, count):
+def count_can_contain_a_valid_bag(valid_bags: List[str], count: int) -> int:
     for b in valid_bags:
         for outer, inner in bags.items():
             if any(b in innerBags for innerBags in inner) and outer not in valid_bags:
@@ -31,3 +37,12 @@ def count_can_contain_a_valid_bag(valid_bags, count):
     return count
 
 print(count_can_contain_a_valid_bag(['shiny gold'], 0))
+
+def count_num_bags_inside(bag: List[Tuple[int, str]], sum: int) -> int:
+    for (num, name) in bag:
+        # print(f"{num} {name}")
+        inner_count = count_num_bags_inside(bags[name], 1)
+        sum += num * inner_count
+    return sum
+    
+print(count_num_bags_inside(bags["shiny gold"], 0))
