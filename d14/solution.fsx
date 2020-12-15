d7dbe6dda714ac1fa2088eff168d684032307ffa
joshspicer <hello@joshspicer.com>
2020-12-15 13:27:28 +0000

    d14 day 1 for one block working

--- d14/solution.fsx
+++ d14/solution.fsx
@@ -13,13 +13,16 @@ let rec parse_mask idx zeroes_arr ones_arr =
         (zeroes_arr, ones_arr)
     else
         match rawMask.[idx] with
-            | '1' -> parse_mask (idx - 1)  zeroes_arr   ( (2F ** (float32 (rawMask.Length - idx - 1)) ) :: ones_arr)
-            | '0' -> parse_mask (idx - 1)  ( (2F ** (float32 (rawMask.Length - idx - 1)) )  :: zeroes_arr)   ones_arr
+            | '1' -> parse_mask (idx - 1)  zeroes_arr   (int (2F ** (float32 (rawMask.Length - idx - 1)) ) :: ones_arr)
+            | '0' -> parse_mask (idx - 1)  (int (2F ** (float32 (rawMask.Length - idx - 1)) )  :: zeroes_arr)   ones_arr
             | _   -> parse_mask (idx - 1)  zeroes_arr   ones_arr
 
-let mask = parse_mask (rawMask.Length - 1) [] []
 
-let instruction =
+// (zero_masks, ones_masks)
+// let mask = parse_mask (rawMask.Length - 1) [] []
+
+// (memory_offset, value)
+let instructions =
     rawInput.Tail
             |> Seq.map(fun x -> 
 
@@ -28,3 +31,17 @@ let instruction =
                 let value = m.Groups.[2]
                 (int offset.Value, int value.Value))
                     |> Seq.toList
+let results: Map<int, int> = Map.empty
+
+// formula: (0 | ONES) & ~(ZEROS)
+let part1 =
+    instructions
+        |> Seq.map(fun (offset, value) ->
+            let apply_ones_results = Seq.fold (fun output x -> output ||| x ) value (snd mask)
+            let apply_both_results = Seq.fold (fun output x -> output &&& ~~~(x)) apply_ones_results (fst mask)
+            (offset, apply_both_results)
+        )
+            |> Seq.fold (fun (map: Map<int,int>) (off, v) -> map.Add (off, v)) results
+
+part1
+    |> Seq.fold (fun sum item -> sum + item.Value) 0 
