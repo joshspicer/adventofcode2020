f11af53fa43bc98a615fab7294897ff4a42dd352
joshspicer <hello@joshspicer.com>
2020-12-15 14:44:47 +0000

    day 14 part 1 corrected

--- d14/solution.fsx
+++ d14/solution.fsx
@@ -5,17 +5,15 @@ open System.Text.RegularExpressions
 let fileName = "input"
 let rawInput = File.ReadAllText($"d14/{fileName}").Split("\n") |> Seq.toList
 
-
 let rec parse_mask (string_mask: string) idx zeroes_arr ones_arr = 
     // printfn "%d" idx
     if idx < 0 then
         (zeroes_arr, ones_arr)
     else
         match string_mask.[idx] with
-            | '1' -> parse_mask string_mask (idx - 1)  zeroes_arr   (int (2F ** (float32 (string_mask.Length - idx - 1)) ) :: ones_arr)
-            | '0' -> parse_mask string_mask (idx - 1)  (int (2F ** (float32 (string_mask.Length - idx - 1)) )  :: zeroes_arr)   ones_arr
+            | '1' -> parse_mask string_mask (idx - 1)  zeroes_arr   (int64 (2F ** (float32 (string_mask.Length - idx - 1)) ) :: ones_arr)
+            | '0' -> parse_mask string_mask (idx - 1)  (int64 (2F ** (float32 (string_mask.Length - idx - 1)) )  :: zeroes_arr)   ones_arr
             | _   -> parse_mask string_mask (idx - 1)  zeroes_arr   ones_arr
-
 
 let parse_instructions =
     rawInput
@@ -30,15 +28,15 @@ let parse_instructions =
                         
                     else
                         let m = Regex.Match(token, "mem\[(\d.*)\] = (\d.*)") 
-                        let offset = int m.Groups.[1].Value
-                        let value = int m.Groups.[2].Value
+                        let offset = int64 m.Groups.[1].Value
+                        let value = int64 m.Groups.[2].Value
                         let head = instructions.Head
                         let mask = (fst head)
                         let previous_instructs = (snd head)
                         (mask,  previous_instructs @ [(offset, value)]) :: instructions.Tail                      
         ) []
 
-let apply (prevMap: Map<int,int>) (input: (list<int> * list<int>) * list<int * int>) =
+let apply prevMap input =
     let zeros = fst (fst input)
     let ones = snd (fst input)
     let instructions = snd input
@@ -46,12 +44,13 @@ let apply (prevMap: Map<int,int>) (input: (list<int> * list<int>) * list<int * i
         |> Seq.map(fun input ->
             let offset = fst input
             let value = snd input
-            let apply_ones_results = Seq.fold (fun output x -> output ||| x ) value ones
+            let apply_ones_results = Seq.fold (fun output (x :int64) -> output ||| x ) value ones
             let apply_both_results = Seq.fold (fun output x -> output &&& ~~~(x)) apply_ones_results zeros
             (offset, apply_both_results)
         )
-            |> Seq.fold (fun (map: Map<int,int>) (off, v) -> map.Add (off, v)) prevMap
+            |> Seq.fold (fun (map: Map<int64,int64>) (off, v) -> map.Add (off, v)) prevMap
 
 parse_instructions
+    |> Seq.rev
     |> Seq.fold (fun map item -> apply map item) Map.empty 
-    |> Seq.fold (fun sum resultsMap -> sum + resultsMap.Value) 0
+    |> Seq.fold (fun sum resultsMap -> sum + resultsMap.Value) 0L
